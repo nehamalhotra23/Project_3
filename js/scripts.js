bfca91334af8f24cbc586087d8ad7363c2bf5ecf
Neha Malhotra <nehamalhotra8899@gmail.com>
2019-08-23 13:24:11 -0700

    trying to fix repetition of output and jquery

--- js/scripts.js
+++ js/scripts.js
@@ -3,56 +3,39 @@
 $(document).ready(function() {
   $(".formOne").submit(function(event) {
     event.preventDefault();
-
     var userInput = $("input#numBer").val();
-if (userInput < 0) {
+    var result = numberToString(userInput);
+    $("#answer").text(result);
+    $("#formOne").trigger('reset');
+    if (userInput < 0) {
       alert('please enter a positive number');
     } else {
       numberToString(userInput);
     }
-
   });
+
 });
 
 //Business Logic
 
-function numberToString(userInput) {
-var birdOutput = [];
+ function numberToString(userInput) {
+  var birdOutput = [];
 if (userInput.includes(1)) {
   $("#answer").append("<li>" + 'Beep!' + "</li>");
-} if (userInput.includes(2)) {
-$("#answer").append("<li>" + 'Boop!' + "</li>");
-}   if (userInput.includes(3)){
+}
+if (userInput.includes(2)) {
+  $("#answer").append("<li>" + 'Boop!' + "</li>");
+}
+if (userInput.includes(3)) {
   $("#answer").append("<li>" + 'I am sorry, Dave. I am afraid I can\'t do that!' + "</li>")
-} else {
-  alert();
+} if (for (var i = 0; i < userInput.length; i++) {
+
+})
+
 }
-}
 
 
 
 
 
-
-
-
-
-
-//   // for (var i = 0; i <= userInput; i++) {
-//   //   birdOutput.push(i.toString());
-//   // }
-// for (var i = 0; i < outputArray.length; i++) {
-//
-//
-
-
-//   var birdOutput = [];
-//   for (var i = 0; i < birdOutput.length; i++) {
-//     if (birdOutput[i].includes(3)) {
-//       birdOutput[i] = "I'm sorry, Dave, I'm afraid I can't do that";
-//     } else if (birdOutput[i][i].includes(2)) {
-//       birdOutput[i] = "Boop!";
-//     } else if (birdOutput[i][i].includes(1)) {
-//       birdOutput[i] = "Beep!";
-//     }
-//   }
+  
