ff563784d40618a4d6a81aebaff99133fae2b2a5
Neha Malhotra <nehamalhotra8899@gmail.com>
2019-08-23 15:15:50 -0700

    fixing output of function on webpage

--- js/scripts.js
+++ js/scripts.js
@@ -1,70 +1,37 @@
-// //User Interface Logic
-//
-// $(document).ready(function() {
-//   $(".formOne").submit(function(event) {
-//     event.preventDefault();
-//     var userInput = $("input#numBer").val();
-//     // var result = numberToString(userInput);
-//     // $("#answer").text(result);
-//     $("#formOne").trigger('reset');
-//     if (userInput < 0) {
-//       alert('please enter a positive number');
-//     } else {
-//       numberToString(userInput);
-//     }
-//   });
-//
-// });
-//
-//Business Logic
 
-//  function numberToString(userInput) {
-//   var birdOutput = [];
-// if (userInput[1]=== '1'){
-//   $("#answer").append("<li>" + 'Beep!' + "</li>");
-// }
-// if (userInput[1] === '2') {
-//   $("#answer").append("<li>" + 'Boop!' + "</li>");
-// }
-// if (userInput[1] === '3') {
-//   $("#answer").append("<li>" + 'I am sorry, Dave. I am afraid I can\'t do that!' + "</li>")
-// } if (userInput[0] ) {
-//
-// }
-//
-// }
-
-//This is the User logic//
+//User Interface Logic
 
 $(document).ready(function() {
   $(".formOne").submit(function(event) {
     event.preventDefault();
-    var userInput = parseInt($("#numBer").val());
-    var output = beep(userInput);
-    var outputString = output.join("");
-    // $("#answers").show();
-    $("#answers").text(outputString);
+    var userInput = parseInt($("input#numBer").val());
+    var results = boopTobeep(userInput);
+    $("#answer").append("<li>" + userInput + "</li>");
     if (userInput < 0) {
       alert('please enter a positive number');
     } else {
-       beep(userInput);
+      boopTobeep(userInput);
     }
-
   });
+
 });
 
-var beep = function(userInput) {
-  var results = [];
-  for (var i = 0; i <= userInput; i++) {
-    var newString = (i).toString();
+//Business Logic
 
-    if (newString.includes("1")) {
-      results.push("Beep");
-    } else if (newString.includes("2")) {
-      results.push("Boop!");
-    } else if (newString.includes("3")) {
-      results.push("I'm sorry, Dave. I'm afraid I can't do that.");
-    }
+function boopTobeep(userInput) {
+  var birdOutput = [];
+for (var i = 0; i <= userInput; i++) {
+  if ([i].includes(1)) {
+    birdOutput.push("Beep");
+
+  } else if ([i].includes(2)) {
+    birdOutput.push("Boop!");
+  } else if ([i].includes(3)) {
+    birdOutput.push("I'm sorry, Dave. I'm afraid I can't do that.");
+  } else {
+
   }
-  return results;
+
 }
+}
+boopTobeep();
