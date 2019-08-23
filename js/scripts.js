882485d0fa6b3b858f385b8ac656ad704d46c10e
Neha Malhotra <nehamalhotra8899@gmail.com>
2019-08-23 10:56:39 -0700

    adding jquery for 1, 2, 3 numbers

--- js/scripts.js
+++ js/scripts.js
@@ -1,19 +1,42 @@
-$(document).ready(function(){
-$(".formOne").submit(function(){
-  event.preventDefault();
-var userInput = parseInt($("input#numBer").val());
-console.log(userInput)
-var Num1 = [1 , 2, 3];
-console.log(Num1);
- var newArray = [];
-for (var i = 0; i < userInput; i++)
- console.log(userInput[i])
-if (Num1.includes(userInput[i])) {
-   newArray.push('beep');
+//User Interface Logic
 
+$(document).ready(function() {
+  $(".formOne").submit(function(event) {
+    event.preventDefault();
+    var userInput = $("input#numBer").val();
+    // var newArray = $("#answer").append("<li>" + userInput + "</li>")
+    var bird = []
+    if (userInput < 0) {
+      alert('please enter a positive number');
+    } else {
+      numberToString(userInput);
+    }
+
+  });
+});
+
+//Business Logic
+
+function numberToString(userInput) {
+  if (userInput.includes(1)) {
+    $("#answer").append("<li>" + 'Boo!' + "</li>")
+  } else  {
+    twoToBoo(userInput);
+  }
 }
 
-});
+function twoToBoo(userInput) {
+  if (userInput.includes(2)) {
+    $("#answer").append("<li>" + 'Boop!' + "</li>")
+  } else {
+    threeToDamn(userInput);
+  }
+}
 
-
-});
+function threeToDamn(userInput) {
+  if(userInput.includes(3)) {
+  $("#answer").append("<li>" + 'I am sorry, Dave. I am afraid I can\'t do that!' + "</li>")
+} else  {
+  alert();
+}
+}
