0fb676a25fd80c1a0205d9ff94bff7a9eb65fc35
Neha Malhotra <nehamalhotra8899@gmail.com>
2019-08-27 19:30:03 -0700

    fixing if else statement and loop

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,4 @@
 //User Interface Logic
-
 $(document).ready(function() {
   $(".formOne").submit(function(event) {
     event.preventDefault();
@@ -11,32 +10,25 @@ $(document).ready(function() {
     }
     $("#answers").text(display);
   });
+  $(".formOne").trigger("reset");
 });
 
-//Business Logic
 
 function boopTobeep(userInput) {
   var birdOutput = [];
-  for (var i = 0; i <= userInput; i++) {
-    if ([i].includes(0)) {
-      birdOutput.push("0");
-    } else if ([i].includes(1)) {
-      birdOutput.push("Beep");
-    } else if ([i].includes(2)) {
+  for (var i = 0; i <= userInput; i++ ) {
+   var inputString = (i).toString();
+     if (inputString.includes("1")) {
+      birdOutput.push("Beep!");
+    } else if (inputString.includes("2")) {
       birdOutput.push("Boop!");
-    } else if ([i].includes(3)) {
+    } else if (inputString.includes("3")) {
       birdOutput.push("I'm sorry, Dave. I'm afraid I can't do that");
-    } else if ([i].includes(4)) {
-      birdOutput.push("4");
-    } else if ([i].includes(5)) {
-      birdOutput.push("5");
+    }  else {
+      birdOutput.push(inputString);
     }
+
+
   }
   return birdOutput;
 }
-boopTobeep();
-
-function autoRefreshPage() {
-  window.location = window.location.href;
-}
-setInterval('autoRefreshPage()', 7000);
